13e79dfef5bc24390c7394c718ae94d6f6d24191
BabishShrestha <babishshrestha8@gmail.com>
2022-01-03 00:33:23 +0545

    Adding the UI of API call . as well as declaring constants

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:core';
+
+import 'package:api_fetch/shared/constants.dart';
+import 'package:api_fetch/shared/fetchapi.dart';
+import 'package:flutter/material.dart';
+
+import 'models/api_display_model.dart';
+
+void main() {
+  runApp(const ApiCall());
+}
+
+class ApiCall extends StatelessWidget {
+  const ApiCall({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: kAppBarColorbg,
+          foregroundColor: kAppBarColorfg,
+          title: Text('Api Call Check'),
+        ),
+        body: SafeArea(
+          child: ApiCheck(),
+        ),
+      ),
+    );
+  }
+}
+
+class ApiCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      margin: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("id"),
+              Text("Title"),
+              Text("Status"),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: FutureBuilder<List>(
+              future: FetchApi(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemExtent: 50,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data?.length ?? 0,
+                    itemBuilder: (BuildContext context, int index) {
+                      Map post = snapshot.data![index];
+                      return Api_Display(
+                          id: post["id"],
+                          title: post["title"],
+                          status: post["completed"]);
+                    },
+                  );
+                }
+                return Center(
+                  child: Transform.scale(
+                    scale: 3,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                      color: kAppBarColorbg,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final kAppBarColorbg = Colors.yellow;
+final kAppBarColorfg = Colors.black;
